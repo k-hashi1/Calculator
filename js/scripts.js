cec3811c62c77376284decfe3f5a52d6b512939b
AlexCFreeman <Alex.C.Freeman@gmail.com>
2020-09-08 18:39:51 -0700

    add js functions and html framework

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function add(number1,number2){return number1+number2;}
+function subtract(number1,number2){return number1-number2;}
+function multiply(number1,number2){return number1*number2;}
+function divide(number1,number2){return number1/number2;}
+const number1=parseInt(prompt("Enter a number"));
+const number2=parseInt(prompt("Enter another number"));
+alert(divide(number1,number2));
